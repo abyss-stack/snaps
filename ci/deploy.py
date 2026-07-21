db2391d13e220e2b45e706f712e1e9cfe34b0d79
responsecode502 <responsecode502@gmail.com>
2026-07-21 18:59:54 +0300

    ci-2026-07-21 18:59:54.555546

--- ci/deploy.py
+++ ci/deploy.py
@@ -8,14 +8,12 @@ BRANCH = "main"
 if __name__ == "__main__":
     now = datetime.now()
     version = now.strftime("%y.%-m.%-d")
+    project_path = Path(__file__).parent.parent
+
     print(f"Project version: {version}")
-
-    project_path = Path(__file__).parent
-
-    print(project_path)
+    print(f"Project path: {project_path}")
 
     with sh.pushd(project_path):
-        
         sh.git("add", ".")
         sh.git("commit", "-m", f"ci-{now}")
         sh.git("push", "origin", BRANCH)
