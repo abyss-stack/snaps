d3d38d07ec3c15127ce4a128354631a80c031551
responsecode502 <responsecode502@gmail.com>
2026-07-21 19:18:55 +0300

    ci-2026-07-21 19:18:55.031869

--- ci/deploy.py
+++ ci/deploy.py
@@ -9,9 +9,14 @@ if __name__ == "__main__":
     now = datetime.now()
     version = now.strftime("%y.%-m.%-d")
     project_path = Path(__file__).parent.parent
+    cargo_toml = project_path / "Cargo.toml"
 
     print(f"Project version: {version}")
     print(f"Project path: {project_path}")
+
+    doc = tomlkit.parse(cargo_toml.read_text(encoding="utf-8"))
+    doc["package"]["version"] = version
+    cargo_toml.write_text(tomlkit.dumps(doc), encoding="utf-8")
 
     sh.cargo("check")
     
